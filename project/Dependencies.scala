90fa1ebee5a68a64f8dd4d49aafa0d732f4ce355
Heiko Seeberger <mail@heikoseeberger.de>
2016-03-10 12:40:37 +0100

    Add support for ConstructR and Docker

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Version {
   final val AkkaLog4j    = "1.1.2"
   final val AkkaSse      = "1.6.3"
   final val Circe        = "0.3.0"
-  final val Constructr   = "0.9.1"
+  final val Constructr   = "0.11.1"
   final val Log4j        = "2.5"
   final val Scala        = "2.11.8"
   final val ScalaTest    = "2.2.6"
@@ -22,6 +22,7 @@ object Library {
   val akkaSse              = "de.heikoseeberger"        %% "akka-sse"                % Version.AkkaSse
   val akkaTestkit          = "com.typesafe.akka"        %% "akka-testkit"            % Version.Akka
   val circeGeneric         = "io.circe"                 %% "circe-generic"           % Version.Circe
+  val constructrAkka       = "de.heikoseeberger"        %% "constructr-akka"         % Version.Constructr
   val log4jCore            = "org.apache.logging.log4j" %  "log4j-core"              % Version.Log4j
   val scalaTest            = "org.scalatest"            %% "scalatest"               % Version.ScalaTest
 }
